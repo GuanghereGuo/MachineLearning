594b7f82f277e3804d2c8f4d237dc7c1d7992160
guanghere <guanghere@mail.nwpu.edu.cn>
2025-08-07 00:17:03 +0800

    commit 0807

--- PackageLearning/test.py
+++ PackageLearning/test.py
@@ -1,11 +1,27 @@
-import os
-from matplotlib.image import imread
+import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+# sns.set_style("ticks")
+# sns.set_palette("muted")
 
-image_path = os.path.join(script_dir, '..', 'DeepLearningFromScratchCode', 'dataset', 'lena_gray.png')
-img = imread(image_path)
-plt.imshow(img)
+def f(x, y):
+    return np.sin(np.sqrt(x**2 + y**2))
 
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+
+fig = plt.figure(figsize=(8, 6), dpi=300)
+ax = fig.add_subplot(111, projection='3d')
+# ax.plot_surface(X, Y, Z, cmap='plasma')
+ax.plot_surface(X, Y, Z, cmap='coolwarm')
+# ax.plot_surface(X, Y, Z, cmap='viridis')
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_zlabel('Z-axis')
+ax.set_title(r'3D Surface Plot of $f(x, y)=\sin(\sqrt{x^2+y^2})$')
 plt.show()
